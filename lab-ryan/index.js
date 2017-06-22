9fed180ead9168b9d76eb1d6dff0fa667c162550
Ryan Turner <ryanthepit@gmail.com>
2017-06-21 21:00:37 -0700

    quit and troll finished

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const net = require('net');
-const user = require('./client.js');
 const server = net.createServer();
 
 let clientPool = [];
-let currentUser = 1;
+let client = function(socket, nickname){
+  this.nickname = nickname;
+  this.socket = socket;
+};
 server.on('connection', (socket) => {
   socket.write('hello socket, welcome to turnchat!/n');
+  socket.nickname = client.nickname;
   console.log('welcome to turnchat');
-  socket.nickname = `user_${currentUser()}`;
-  currentUser++;
-
-  console.log(`${socket.nickname} connected!`);
   clientPool = [...clientPool, socket];
   socket.write('You\'re connected as ${socket.nickname}\n');
+
   let handleDisconnect = () => {
     console.log(`${socket.nickname} has disconnected`);
     clientPool = clientPool.filter(item => item !== socket);
@@ -29,51 +29,39 @@ server.on('connection', (socket) => {
       socket.nickname = data.split('/nick')[1] || socket.nickname;
       socket.nickname = socket.nickname.trim();
       client.nickname = socket.nickname;
-        user.write(`you are now known as ${socket.nickname}\n`);
-        return;
-      }
-        if(data.startsWith('/dm')){
-          let content = data.split('/dm ')[1] || '';
-          content = content.trim;
-          let userName = content.split(' ')[0].trim();
-          client
-          socket.nickname = data.split('/nick')[1] || socket.nickname;
-          socket.nickname = socket.nickname.trim();
-          socket.write(`you are now known as ${socket.nickname}\n`);
-        }
-      };
+      socket.write(`you are now known as ${socket.nickname}\n`);
       return;
     }
-
     if(data.startsWith('/dm')){
-      let content = data.split('/dm')[1] || '';
-      content = content.trim();
-      let to = content.split(' ')[0].trim();
+      let content = data.split('/dm ')[1] || '';
+      content = content.trim;
+      let userName = content.split(' ')[0].trim();
       clientPool.forEach((user) => {
-        if(to == user.nickname){
-          user.socket.write(`${socket.nickname}: ${data.split(to)[1]}\n`);
+        if(userName === user.nickname){
+          user.socket.write(`${socket.nickname}: ${data.split(userName)[1]}\n`);
         }
       });
       return;
     }
 
     if(data.startsWith('/troll')){
-      let content = data.split('/dm')[1] || '';
-      content = content.trim();
-      let to = content.split(' ')[0].trim();
-      clientPool.forEach((user) => {
-        if(to == user.nickname){
-          user.socket.write(`${socket.nickname}: ${data.split(to)[1]}\n`);
-        }
-      });
-      if(data.startsWith('/quit')){
-        let content = data.split('/dm')[1] || '';
-        content = content.trim();
-        let to = content.split(' ')[0].trim();
+      let amount = data.split('/troll ')[1].slice(0,1);
+      let content = data.split(' ').slice(2).join(' ');
+      for (var i = 0; i < amount; i++){
         clientPool.forEach((user) => {
-          if(to == user.nickname){
-            user.socket.write(`${socket.nickname}: ${data.split(to)[1]}\n`);
-          }
+          user.socket.write(`${socket.nickname}: ${content}\n`);
+        });
+      }
+      return;
+    }
+
+    if(data.startsWith('/quit')){
+      clientPool.forEach((user) => {
+        user.socket.write(`\n${socket.nickname} has quit!\n`);
+      });
+      client.socket.end();
+      return;
+    }
     clientPool.forEach((user) => {
       user.socket.write(`${socket.nickname}: ${data}`);
     });
